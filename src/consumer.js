e75a18b5b7e0fc27ad43684f5b6f5c9bd2891b15
caturheri <caturheriprayitnoe@gmail.com>
2024-02-11 16:11:23 +0700

    first commit

---
+++ src/consumer.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const amqp = require('amqplib');
+const PlaylistsService = require('./PlaylistService');
+const MailSender = require('./MailSender');
+const Listener = require('./listener');
+
+const init = async () => {
+  const playlistsService = new PlaylistsService();
+  const mailSender = new MailSender();
+  const listener = new Listener(playlistsService, mailSender);
+
+  const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+  const channel = await connection.createChannel();
+
+  const channelName = 'export:playlists';
+
+  await channel.assertQueue(channelName, {
+    durable: true,
+  });
+
+  channel.consume(channelName, listener.listen, { noAck: true });
+};
+
+init();
